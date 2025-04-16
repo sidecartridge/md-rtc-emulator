e98af8acf4f6576965bc17f8da9196699eb76b38
Diego Parrilla <diego.parrilla.santamaria@gmail.com>
2025-04-16 17:48:07 +0200

    Use watchdog_reboot instead of watchdog_enable

--- rp/src/reset.c
+++ rp/src/reset.c
@@ -2,10 +2,10 @@
 
 void reset_device() {
   DPRINTF("Resetting the device\n");
-  //    watchdog_reboot(0,0,1000);
 
   save_and_disable_interrupts();
-  watchdog_enable(RESET_WATCHDOG_TIMEOUT, 0);
+  // watchdog_enable(RESET_WATCHDOG_TIMEOUT, 0);
+  watchdog_reboot(0, 0, RESET_WATCHDOG_TIMEOUT);
   // 20 ms timeout, for example, then the chip will reset
   while (1) {
     // Wait for the reset
